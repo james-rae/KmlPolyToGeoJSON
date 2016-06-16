0e4ecca1713f2987b95ffc69f6a447c5b444b178
James Rae <james.rae@ec.gc.ca>
2016-06-16 11:05:16 -0400

    it is a thing

---
+++ KML_to_GeoJSON/Form1.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.openSesame = New System.Windows.Forms.OpenFileDialog()
+        Me.cmdEnhance = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'openSesame
+        '
+        Me.openSesame.FileName = "d:\data\LWB\LWB_Raw.txt"
+        '
+        'cmdEnhance
+        '
+        Me.cmdEnhance.Location = New System.Drawing.Point(12, 12)
+        Me.cmdEnhance.Name = "cmdEnhance"
+        Me.cmdEnhance.Size = New System.Drawing.Size(257, 45)
+        Me.cmdEnhance.TabIndex = 0
+        Me.cmdEnhance.Text = "Pick A File And GRIND"
+        Me.cmdEnhance.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 70)
+        Me.Controls.Add(Me.cmdEnhance)
+        Me.Name = "Form1"
+        Me.Text = "Make Me Some GeoJSON"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents openSesame As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents cmdEnhance As System.Windows.Forms.Button
+
+End Class
---
+++ KML_to_GeoJSON/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+    Private Sub cmdEnhance_Click(sender As Object, e As EventArgs) Handles cmdEnhance.Click
+        Me.openSesame.ShowDialog()
+
+
+    End Sub
+
+    Private Sub openSesame_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles openSesame.FileOk
+
+        Dim streamRead As New System.IO.StreamReader(Me.openSesame.OpenFile())
+
+        Dim streamWrite As New System.IO.StreamWriter(Me.openSesame.FileName + "out.txt", False)
+        Dim blockOfText = streamRead.ReadToEnd()
+
+        ' turn wall of text into array of co-ord triplits
+        Dim kmlPoints = blockOfText.Split(" ")
+
+        streamWrite.WriteLine("[[")
+        For Each kPoint In kmlPoints
+            'break triplit into points
+            Dim pointy = kPoint.Split(",")
+            streamWrite.WriteLine("[" & pointy(0) & ", " & pointy(1) & "],")
+        Next
+
+        streamWrite.WriteLine("]]")
+        streamWrite.Close()
+
+        MessageBox.Show("DONE, THANKS")
+    End Sub
+End Class
